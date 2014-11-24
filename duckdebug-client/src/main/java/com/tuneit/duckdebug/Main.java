a1bbd81f2638edc0d5c6b2db717099841b83fdb2
tegoo <tegoo@d7cbc6bd-5246-4375-9b03-89918eec8a7b>
2014-11-24 12:06:48 +0000

    fix
    git-svn-id: svn://sunray.elcom.spb.ru/tuneit/trunk/duckdebug@248 d7cbc6bd-5246-4375-9b03-89918eec8a7b

--- duckdebug-client/src/main/java/com/tuneit/duckdebug/Main.java
+++ duckdebug-client/src/main/java/com/tuneit/duckdebug/Main.java
@@ -2,26 +2,44 @@ package com.tuneit.duckdebug;
 
 import org.apache.commons.cli.*;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static final byte INTERACTIVE_MODE_CODE   = (byte) 0xff;
+    public static final byte FILE_MODE_CODE          = (byte) 0x00;
+
+    public static void main(String[] args) throws IOException {
 
         String host = "";
         int port = 0;
+        String scriptFile = null;
+        String key = "";
 
-        Socket socket;
-        final InputStream is;
-        final OutputStream os;
+
+        Socket socket = null;
+        final InputStream sis;
+        final OutputStream sos;
 
         Options options = new Options();
-        options.addOption("h", "host", true, "server host");
-        options.addOption("p", "port", true, "server port");
+        Option opt;
+
+        opt = new Option("h", "host", true, "server host");
+        opt.setRequired(true);
+        options.addOption(opt);
+
+        opt = new Option("p", "port", true, "server port");
+        opt.setRequired(true);
+        options.addOption(opt);
+
+        opt = new Option("k", "key",  true, "binding key");
+        opt.setRequired(true);
+        options.addOption(opt);
+
+        opt = new Option("f", "file", true, "script file");
+        options.addOption(opt);
 
         boolean failed = false;
         CommandLineParser parser = new BasicParser();
@@ -29,8 +47,12 @@ public class Main {
             CommandLine line = parser.parse( options, args );
             host = line.getOptionValue("h");
             port = Integer.parseInt(line.getOptionValue("p"));
+            key = line.getOptionValue("k");
+            if(line.hasOption("f")) {
+                scriptFile = line.getOptionValue("f");
+            }
         } catch (ParseException exp) {
-            System.err.println( exp.getMessage() );
+            System.err.println(exp.getMessage());
             failed = true;
         } catch (NumberFormatException ex) {
             failed = true;
@@ -45,23 +67,47 @@ public class Main {
 
         try {
             socket = new Socket(InetAddress.getByName(host), port);
-            is = socket.getInputStream();
-            os = socket.getOutputStream();
+            sis = socket.getInputStream();
+            sos = socket.getOutputStream();
+
+            DataOutputStream dos = new DataOutputStream(sos);
+            dos.writeInt(key.getBytes().length);
+            dos.write(key.getBytes());
+
+            if (scriptFile == null) {
+
+                dos.writeByte(INTERACTIVE_MODE_CODE);
+                interactiveMode(sis, sos);
+
+            } else {
+
+                dos.writeByte(FILE_MODE_CODE);
+                fileMode(sis, sos, scriptFile);
+
+            }
+
         } catch (IOException ex) {
             System.err.println(ex.getLocalizedMessage());
-            return;
+        } finally {
+            if(socket != null) {
+                socket.close();
+            }
         }
+    }
 
-        final Thread recive = new Thread(new Runnable() {
+    public static void interactiveMode(
+            final InputStream sis, final OutputStream sos) {
+
+        Thread recive = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    int c;
+                    int s;
+                    byte[] buf = new byte[1024];
                     while (true) {
-                        c = is.read();
-                        if (c == -1) break;
-
-                        System.out.write(c);
+                        s = sis.read(buf);
+                        if (s == -1) break;
+                        System.out.write(buf, 0, s);
                         System.out.flush();
                     }
                 } catch(IOException ex) { }
@@ -76,24 +122,37 @@ public class Main {
                     while (true) {
                         c = System.in.read();
                         if (c == -1) break;
-                        os.write(c);
-                        os.flush();
+                        sos.write(c);
+                        sos.flush();
                     }
                 } catch(IOException ex) { }
             }
         });
 
-
         recive.start();
         send.start();
-
         try {
             recive.join();
             System.in.close();
             send.join();
-            socket.close();
         } catch (InterruptedException | IOException ex) {
-            ex.printStackTrace();
+            System.err.println(ex.getLocalizedMessage());
+        }
+    }
+
+    public static void fileMode(
+            final InputStream sis, final OutputStream sos, String fileName) {
+
+        File file = new File(fileName);
+        byte[] buf = new byte[(int)file.length()];
+
+        try {
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+            bis.read(buf, 0, (int) file.length());
+            sos.write(buf);
+            sos.flush();
+        } catch(IOException ex) {
+            System.err.println(ex.getLocalizedMessage());
         }
     }
 }
--- duckdebug-server/src/main/java/com/tuneit/duckdebug/Server.java
+++ duckdebug-server/src/main/java/com/tuneit/duckdebug/Server.java
@@ -1,15 +1,19 @@
 package com.tuneit.duckdebug;
 
 import groovy.lang.Binding;
+import groovy.lang.Closure;
+import groovy.lang.GroovyShell;
 import org.codehaus.groovy.tools.shell.Groovysh;
 import org.codehaus.groovy.tools.shell.IO;
 import org.fusesource.jansi.Ansi;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -19,15 +23,27 @@ import java.util.concurrent.TimeUnit;
  */
 public class Server {
 
+    public static final byte INTERACTIVE_MODE_CODE   = (byte) 0xff;
+    public static final byte FILE_MODE_CODE          = (byte) 0x00;
+
     private int port;
     private ServerSocket serverSocket;
 
     private int poolSize;
     private ExecutorService workers;
 
-    public Server(int port, int poolSize) {
+    private Map<String, Object> context;
+
+    private Map<String, Binding> bindings = new Hashtable<>();
+
+    public Server(int port, int poolSize, Map<String, Object> context) {
         this.port = port;
         this.poolSize = poolSize;
+        this.context = context;
+    }
+
+    public Server(int port, int poolSize) {
+        this(port, poolSize, new HashMap<String, Object>());
     }
 
     public void start() throws IOException {
@@ -74,29 +90,92 @@ public class Server {
 
         @Override
         public void run() {
-            OutputStream out;
-            InputStream in;
+            final OutputStream sos;
+            final InputStream sis;
 
             try {
-                this.socket.setSoTimeout(60 * 60 * 1000);
-                out = socket.getOutputStream();
-                in = socket.getInputStream();
-                out.write(Ansi.ansi().render("@|YELLOW " + duck + "|@")
-                                .toString().getBytes());
-                out.flush();
-            } catch(IOException ex) {
+                socket.setSoTimeout(60 * 60 * 1000);
+
+                sos = socket.getOutputStream();
+                sis = socket.getInputStream();
+
+                DataInputStream dis = new DataInputStream(sis);
+
+                int keySize = dis.readInt();
+                byte[] keyBuf = new byte[keySize];
+                dis.readFully(keyBuf);
+                String key = new String(keyBuf);
+
+                boolean interactive =dis.readByte() == INTERACTIVE_MODE_CODE;
+
+                final Binding binding;
+                if(bindings.containsKey(key)) {
+                    binding = bindings.get(key);
+                } else {
+                    binding = new Binding();
+                    bindings.put(key, binding);
+                    for (Map.Entry<String, Object> entry : context.entrySet()) {
+                        binding.setProperty(entry.getKey(), entry.getValue());
+                    }
+                    binding.setProperty("startTime", new Date());
+                    binding.setProperty("bindings", bindings);
+                }
+
+                if (interactive) {
+
+                    class DuckClosure extends Closure {
+                        public DuckClosure( Object owner, Object thisObject ) {
+                            super( owner, thisObject ) ;
+                        }
+
+                        public DuckClosure( Object owner ) {
+                            super( owner ) ;
+                        }
+
+                        public Object call() {
+                            try {
+                                sos.write(Ansi.ansi().render("@|YELLOW " + duck + "|@")
+                                        .toString().getBytes());
+                                sos.flush();
+                            } catch(IOException ex) { }
+                            return null;
+                        }
+                    }
+
+                    Groovysh shell = new Groovysh(binding, new IO(sis, sos, sos));
+                    binding.setProperty("out", sos);
+                    binding.setProperty("duck", new DuckClosure(shell));
+                    shell.run("");
+                    
+                } else {
+                    
+                    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    byte[] buf = new byte[1024];
+                    int s;
+                    while((s = sis.read(buf)) != -1) {
+                        baos.write(buf, 0, s);
+                    }
+
+                    Thread execution = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                GroovyShell shell = new GroovyShell(binding);
+                                shell.evaluate(new String(baos.toByteArray()));
+                            } catch(Exception ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    });
+
+                    binding.setProperty("thread", execution);
+                    execution.start();
+                }
+            } catch (IOException ex) {
+
+            } finally {
                 tryClose();
-                return;
             }
-
-            Binding binding = new Binding();
-            Groovysh shell = new Groovysh(binding, new IO(in, out, out));
-            binding.setProperty("out", out);
-            binding.setProperty("shell", shell);
-            binding.setProperty("duck", duck);
-            shell.run("");
-
-            tryClose();
         }
 
         public void tryClose() {
